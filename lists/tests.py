5c4088a6abb6e4bbff1d0c4a93d85fc9ac58bd92
Michael Bateman <michael.bateman.com@gmail.com>
2020-11-02 15:45:04 +0000

    Front page HTML now generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -11,11 +11,9 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()  
