d887a1a5dfcc221308fe796955893f30bc151c38
Michael Bateman <michael.bateman.com@gmail.com>
2020-11-23 21:51:25 +0000

    use form in all views, back to working state

--- lists/migrations/0003_list.py
+++ lists/migrations/0003_list.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('lists', '0002_text'),
+        ('lists', '0002_item_text'),
     ]
 
     operations = [
