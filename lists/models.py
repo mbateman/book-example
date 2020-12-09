58d7aaf215a6ca697dad03683928e4c9a33cfeb8
Michael Bateman <michael.bateman.com@gmail.com>
2020-12-09 21:46:44 +0000

    lists can have owners, which are saved on creation.

--- lists/models.py
+++ lists/models.py
@@ -1,10 +1,13 @@
+from django.conf import settings
 from django.db import models
-
 # Create your models here.
 from django.urls import reverse
 
 
 class List(models.Model):
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
