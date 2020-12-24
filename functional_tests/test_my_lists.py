4f534c0d39609d644bb332e82ad0b710f08e2fda
Michael Bateman <michael.bateman.com@gmail.com>
2020-12-24 08:31:52 +0000

    New FT for sharing, move session creation stuff to base

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,34 +1,10 @@
-from django.conf import settings
-from selenium.webdriver.common.keys import Keys
-
 from .base import FunctionalTest
-from .server_tools import create_session_on_server
-from .management.commands.create_session import create_pre_authenticated_session
+
+
 # User = get_user_model()
 
 
 class MyListsTest(FunctionalTest):
-
-    def create_pre_authenticated_session(self, email):
-        if self.staging_server:
-            session_key = create_session_on_server(self.staging_server, email)
-        else:
-            session_key = create_pre_authenticated_session(email)
-        ## to set a cookie we need to first visit the domain.
-        ## 404 pages load the quickest!
-        self.browser.get(self.live_server_url + "/404_no_such_url/")
-        self.browser.add_cookie(dict(
-            name=settings.SESSION_COOKIE_NAME,
-            value=session_key,
-            path='/',
-        ))
-
-    def add_list_item(self, item_text):
-        num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
-        self.get_item_input_box().send_keys(item_text)
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        item_number = num_rows + 1
-        self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         # Edith is a logged-in user
