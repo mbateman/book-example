b5ed51404c015fec6d81ac6f1fca169b57f06c25
Michael Bateman <michael.bateman.com@gmail.com>
2020-12-31 19:19:19 +0000

    adding venv

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/michael/python/projects/tdd_with_python/book-example/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
